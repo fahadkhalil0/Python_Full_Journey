21d5c7ec21f1aa180ccce41f75a198faa62ea776
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-26 17:12:55 +0500

    Tested the technology of an isolated virtual evironment for the packages and various modules.

---
+++ Chapter13/01_venv.py
@@ -0,0 +1,17 @@
+#Virtual Environment in Python
+
+#Step 1- Install the virtual environment in terminal by using the command (pip install virttualenv)
+#Step-2 Activate the Virtual Environment in terminal by using the command (source <env_name>/bin/activate)
+#step-3 You can deactive by using deactivate keyword in terminal
+#step-4 If you want to see the installed package use (pip freeze)
+#step-5 Want to save the packages in a txt file then type pip freeze > filename.txt
+#step-6 You can import the packages by using the import package_name
+
+
+# 1- pip install virtualenv
+# 2- virtualenv (venv/any projectfile name)
+# 3- .\path\Scripts\activate.ps1
+# 4- deactivate
+# 5- import pandas
+# 6- pandas.__version__ to chk the installed version
+# 7- This commands will create an isolated environment in your IDE
