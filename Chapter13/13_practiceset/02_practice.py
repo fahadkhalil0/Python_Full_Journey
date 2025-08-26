c86f5ed74f8a901dd6919298e05b9df98ffd5155
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-26 17:20:05 +0500

    Format method used

---
+++ Chapter13/13_practiceset/02_practice.py
@@ -0,0 +1,8 @@
+#Practice task no 02
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+phone = input("Enter your phone number: ")
+
+Format = "My name is {}, my age is {} and my phone number is {}".format(name, age, phone)
+print(Format)
