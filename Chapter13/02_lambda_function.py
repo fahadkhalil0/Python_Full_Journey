15d78ff9b03a09ef7a953415e23d24be5bbfefcf
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-26 17:13:35 +0500

    Explored the advanced concept (lamda function) in this chapter

---
+++ Chapter13/02_lambda_function.py
@@ -0,0 +1,9 @@
+#Lambda function in python
+
+# def sum(a,b):
+#     return a+b
+
+
+#Same function using Lambda == function created using expression
+sum = lambda a,b,: a+b #shortcut of function
+print(sum(10,20))
